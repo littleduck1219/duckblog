a0d3738d709fd8ae0713f0683198d8f15a5b87e2
littleduck <littleduck1219@gmail.com>
2024-07-24 01:33:09 +0900

    reset project & init
    - project re init
    - set yarn berry

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-/** @type {{output: string, assetPrefix: string, experimental: {appDir: boolean}}} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  assetPrefix: "/duckblog/",
-};
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
--- src/app/post/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = async ({ params }: any) => {
-  return <div></div>;
-};
-
-export default Page;
