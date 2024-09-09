e99eed72dc2bbadd33a6c7006e2f1d0fce9f22b0
littleduck <107936957+littleduck1219@users.noreply.github.com>
2024-09-09 16:05:27 +0900

    manifest

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true, // 추가 옵션을 원하면 여기에 정의
+};
+
+export default withPWA({
+    dest: 'public', // PWA 파일을 public 폴더에 저장
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development', // 개발 환경에서 PWA 비활성화
+})(nextConfig);
